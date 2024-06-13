1693a9f0c81223102e1381d0428c0539189fbd84
parsher <dogukanalpt@gmail.com>
2024-06-13 20:48:30 +0300

    Scripts

--- PressShiftToRun.lua
+++ PressShiftToRun.lua
@@ -3,10 +3,14 @@ local userInput = game:GetService("UserInputService")
 local players = game:GetService("Players")
 
 
- 
+
 local sprintSpeed = 25 --oyundaki herkesin shift bastıktan sonraki hızı
 
 local walkSpeed = 16 --oyundaki herkesin yürüyüş hızı
+
+
+
+local sprinting = false
 
 
 
@@ -24,7 +28,19 @@ local function beginSprint(input, gameProcessed)
 
 			if keycode == Enum.KeyCode.LeftShift then 
 
-				player.Character.Humanoid.WalkSpeed = sprintSpeed
+				sprinting = not sprinting
+
+				if sprinting then
+
+					player.Character.Humanoid.WalkSpeed = sprintSpeed
+
+				else
+
+					player.Character.Humanoid.WalkSpeed = walkSpeed
+
+				end
+
+
 
 			end
 
@@ -35,29 +51,7 @@ local function beginSprint(input, gameProcessed)
 end
 
 
-
-local function endSprint(input, gameProcessed)
-
-	if not gameProcessed then
-
-		if input.UserInputType == Enum.UserInputType.Keyboard then
-
-			local keycode = input.KeyCode
-
-			if keycode == Enum.KeyCode.LeftShift then
-
-				player.Character.Humanoid.WalkSpeed = walkSpeed
-
-			end
-
-		end
-
-	end
-
-end
 
 
 
 userInput.InputBegan:Connect(beginSprint)
-
-userInput.InputEnded:Connect(endSprint)
